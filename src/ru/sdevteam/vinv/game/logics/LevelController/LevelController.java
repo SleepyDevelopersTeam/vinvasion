d0afc502e11ad5e1fbfa5ca5fc0af463b54fe258
Ivan <justworkkit@gmail.com>
2015-02-10 02:16:18 +0300

    improve visual styiling of code

--- src/ru/sdevteam/vinv/game/logics/LevelController/LevelController.java
+++ src/ru/sdevteam/vinv/game/logics/LevelController/LevelController.java
@@ -9,7 +9,8 @@ import ru.sdevteam.vinv.game.Level;
 import ru.sdevteam.vinv.game.Tower;
 import ru.sdevteam.vinv.ui.*;
 
-public class LevelController implements IUpdatable, IDrawable {
+public class LevelController implements IUpdatable, IDrawable 
+{
 	private Level modelOfLevel;
 
 	LevelController(Level l) 
@@ -44,7 +45,8 @@ public class LevelController implements IUpdatable, IDrawable {
 	public void update() 
 	{
 		GameObject[] arrayOfGameObjects = modelOfLevel.getBugObjects();
-		for (int i = 0; i < arrayOfGameObjects.length; i++) {
+		for (int i = 0; i < arrayOfGameObjects.length; i++) 
+		{
 			arrayOfGameObjects[i].update();
 		}
 
@@ -52,28 +54,32 @@ public class LevelController implements IUpdatable, IDrawable {
 		Bug[] arrayOfBugs = this.modelOfLevel.getBugObjects();
 		// Bullet[] ArrayOfBullets=this.modelOflevel.getBulletObjects();
 
-		for (int i = 0; i < arrayOfBullets.length; i++) {
-			for (int j = 0; j < arrayOfBugs.length; i++) {
-				if (arrayOfBullets[i].getSprite().collidesWith(
-						arrayOfBugs[j].getSprite())) {
+		for (int i = 0; i < arrayOfBullets.length; i++) 
+		{
+			for (int j = 0; j < arrayOfBugs.length; i++) 
+			{
+				if (arrayOfBullets[i].getSprite().collidesWith(arrayOfBugs[j].getSprite())) 
+				{
 					arrayOfBugs[j].hit(arrayOfBullets[i]);
 					modelOfLevel.disposeBullet(arrayOfBullets[i]); // !!!!!!!
 				}
 			}
 		}
-		for (int i = 0; i < arrayOfTowers.length; i++) {
-			for (int j = 0; j < arrayOfBugs.length; i++) {
+		for (int i = 0; i < arrayOfTowers.length; i++) 
+		{
+			for (int j = 0; j < arrayOfBugs.length; i++) 
+			{
 				// if
 				// (ArrayOfTowers[i].getShootingRadius()>=((ArrayOfBugs[j].getX()-ArrayOfTowers[i].getX())*(ArrayOfBugs[j].getX()-ArrayOfTowers[i].getX()+(ArrayOfBugs[j].getY()-ArrayOfTowers[i].getY())*(ArrayOfBugs[j].getY()-ArrayOfTowers[i].getY()))))
 				// //bug into radius of Tower )
 				{
-					if (arrayOfTowers[i].canShoot()) {
+					if (arrayOfTowers[i].canShoot()) 
+					{
 						// ArrayOfTowers[i].
 						Bullet bullet = null;
 						bullet.convertTo(arrayOfTowers[i].getBulletType());
 						bullet.setX(arrayOfTowers[i].getX());
 						bullet.setY(arrayOfTowers[i].getY());
-
 					}
 				}
 			}
