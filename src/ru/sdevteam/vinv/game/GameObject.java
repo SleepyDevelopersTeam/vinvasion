c793b486bd583f2ddb2e07c4383c603102b7af6d
SolidJack <faizullin.anton@gmail.com>
2015-02-13 01:55:54 +0300

    change koordinatu

--- src/ru/sdevteam/vinv/game/GameObject.java
+++ src/ru/sdevteam/vinv/game/GameObject.java
@@ -15,23 +15,23 @@ public abstract class GameObject implements ru.sdevteam.vinv.ui.IUpdatable,
     
     
     public float getX(){
-        return objSprite.getX();
+        return (objSprite.getX()+objSprite.getWidth()/2);
     }
     
     public float getY(){
-        return objSprite.getY();
+        return (objSprite.getY()+objSprite.getHeight()/2);
     }
 
     public void setX(float nx){
-        objSprite.setX(nx);
+        objSprite.setX(nx-objSprite.getWidth()/2);
     }
 
     public void setY(float ny){
-        objSprite.setY(ny);
+        objSprite.setY(ny-objSprite.getHeight()/2);
     }
 
     public void moveTo(float nx, float ny){
-        objSprite.moveTo(nx,ny);
+        objSprite.moveTo(nx-objSprite.getWidth()/2,ny-objSprite.getHeight()/2);
     }
 
     public void moveBy(float dx, float dy){
