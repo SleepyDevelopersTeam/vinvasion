61156782107cd8c448bb8a0b76beccabbb1bdc2e
tommy0 <tommy0@mail.ru>
2015-02-13 02:17:26 +0300

    add screens

---
+++ src/ru/sdevteam/vinv/ui/LoadingScreen.java
@@ -0,0 +1,23 @@
+package ru.sdevteam.vinv.ui;
+
+import java.awt.Graphics;
+
+public class LoadingScreen extends Screen
+{
+	boolean isLoadingFinish()
+	{
+		return true;
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+}
