cf3e8f5c59fb5bbaceadb6bfe19853eb630a5151
SolidJack <faizullin.anton@gmail.com>
2015-02-15 11:07:00 +0300

    chanhe pool

--- src/ru/sdevteam/vinv/game/Level.java
+++ src/ru/sdevteam/vinv/game/Level.java
@@ -89,71 +89,99 @@ public class Level
 
 class Pool
 {
-	private Bullet[] pool;
-	private boolean[] used;
-	private int created;
-	public int getSize() { return created; }
-	public int getMaxSize() { return pool.length; }
+    private Bullet[] pool;
+    private boolean[] used;
+    private int created;
 
-	public Pool(int maxSize)
-	{
-		pool=new Bullet[maxSize];
-		used=new boolean[maxSize];
-		created=0;
-	}
+    public int getSize()
+    {
+        return created;
+    }
 
-	public Bullet getNewObject(Bullet obj)
-	{
-		for(int i=0; i<created; i++)
-		{
-			if(!used[i])
-			{
-				used[i]=true;
-				//EventBroker.invoke("debugMsgChanged", "returned "+i);
-				return pool[i];
-			}
-		}
-		if(created==pool.length)
-		{
-			//mark everything as unused, except the first that would be returned
-			//that should make all first objects to disappear and to be converted to latest objs
-			for(int i=1; i<pool.length; i++)
-			{
-				used[i]=false;
-			}
-			return pool[0];
-		}
-		used[created]=true;
-                pool[created]=obj;
-		//EventBroker.invoke("debugMsgChanged", "created "+created);
+    public int getMaxSize()
+    {
+        return pool.length;
+    }
 
-		return pool[created++];
-	}
 
-	public void dispose(Bullet obj)
-	{
-		for(int i=0; i<created; i++)
-		{
-			if((Bullet)pool[i]==obj)
-			{
-				used[i]=false;
-				return;
-			}
-		}
-	}
+    public Pool(int maxSize)
+    {
+        pool = new Bullet[maxSize];
+        used = new boolean[maxSize];
+        created = 0;
+    }
 
-	public void dispose()
-	{
-		for(int i=0; i<created; i++)
-		{
-			pool[i]=null;
-		}
-		pool=null;
-                used=null;
-	}
 
-	public Bullet[] getArray()
-	{
-		return pool;
-	}
+    public Bullet getNewObject(Bullet obj)
+    {
+        for (int i = 0; i < created; i++)
+        {
+            if (!used[i])
+            {
+                used[i] = true;
+                //EventBroker.invoke("debugMsgChanged", "returned "+i);
+                return pool[i];
+            }
+        }
+        if (created == pool.length)
+        {
+            //mark everything as unused, except the first that would be returned
+            //that should make all first objects to disappear and to be converted to latest objs
+            for (int i = 1; i < pool.length; i++)
+            {
+                used[i] = false;
+            }
+            return pool[0];
+        }
+        used[created] = true;
+        pool[created] = obj;
+        //EventBroker.invoke("debugMsgChanged", "created "+created);
+
+        return pool[created++];
+    }
+
+    public void dispose(Bullet obj)
+    {
+        for (int i = 0; i < created; i++)
+        {
+            if ((Bullet) pool[i] == obj)
+            {
+                used[i] = false;
+                return;
+            }
+        }
+    }
+
+    public void dispose()
+    {
+        for (int i = 0; i < created; i++)
+        {
+            pool[i] = null;
+        }
+        pool = null;
+        used = null;
+    }
+
+    public Bullet[] getArray()
+    {
+        int count=0;
+        int j=0;
+        for(int i=0;i<pool.length;i++)
+        {
+            if (used[i]=true)
+            {
+                count++;
+            }
+        }
+        Bullet[] mass=new Bullet[count];
+        for(int i=0;i<pool.length;i++)
+        {
+            if (used[i]=true)
+            {
+                mass[j]=pool[i];
+                j++;
+            }
+        }
+        return mass;
+    }
 }
