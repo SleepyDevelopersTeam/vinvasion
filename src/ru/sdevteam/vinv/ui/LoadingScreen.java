954e0aaf334086d0c92d490219f4f3a93be4b270
tommy0 <tommy0@mail.ru>
2015-03-13 01:00:56 +0300

    delete sysout

--- src/ru/sdevteam/vinv/ui/LoadingScreen.java
+++ src/ru/sdevteam/vinv/ui/LoadingScreen.java
@@ -24,8 +24,6 @@ public class LoadingScreen extends Screen
 		splash=null;
 		hightCanvas = loadingCanvas.getHeight();
 		widthCanvas = loadingCanvas.getWidth();
-		System.out.println(hightCanvas+" "+widthCanvas);
-
 	}
 
 	@Override
