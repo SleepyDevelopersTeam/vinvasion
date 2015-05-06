7dd82329536b7f6b03d3dd4632ce21f665301264
Andrey Izmaylov <whiteshaq41@gmail.com>
2015-05-06 21:50:04 +0300

    Added Profile class

--- src/ru/sdevteam/vinv/main/Profile.java
+++ src/ru/sdevteam/vinv/main/Profile.java
@@ -1,5 +1,27 @@
 package ru.sdevteam.vinv.main;
 
-public class Profile {
-
+public class Profile 
+{
+	private String name;
+	private int level;
+	private Profile activeProfile;
+	
+	Profile (String aName, int aLevel)
+	{
+		level = aLevel;
+		name = aName;
+	}
+	
+	public void setActiveProfile(Profile aProfile)
+	{
+		activeProfile = aProfile;
+	}
+	
+	public Profile getActiveProfile()
+	{
+		return activeProfile;
+	}
+	
+	
+	public static String[] allProfiles;
 }
