a70705038640773a59ce71b91bbb7bc26eb63087
Nikita <nikita-ivanov.mail@yandex.ru>
2015-06-04 13:41:44 +0300

    Added base of Control class and implemented its reaction to mouse events (not tested yet).

---
+++ src/ru/sdevteam/vinv/ui/controls/Control.java
@@ -0,0 +1,171 @@
+package ru.sdevteam.vinv.ui.controls;
+
+import java.awt.Graphics;
+
+import ru.sdevteam.vinv.main.MouseEvent;
+import ru.sdevteam.vinv.ui.IDrawable;
+import ru.sdevteam.vinv.ui.IUpdatable;
+
+public abstract class Control implements IUpdatable, IDrawable
+{
+	//
+	// Координаты
+	//
+	private int x=0, y=0;
+	public int getX() { return x; }
+	public int getY() { return y; }
+	public void setX(int nx) { x=nx; }
+	public void setY(int ny) { y=ny; }
+	public void moveTo(int nx, int ny) { x=nx; y=ny; }
+	//public void moveBy(int dx, int dy) { x+=dx; y+=dy; }
+	
+	//
+	// Размеры
+	//
+	private int w=0, h=0;
+	public int getWidth() { return w; }
+	public int getHeight() { return h; }
+	public void setWidth(int nw) { w=nw; }
+	public void setHeight(int nh) { h=nh; }
+	public void setSize(int nw, int nh) { w=nw; h=nh; }
+	
+	//
+	// Состояние
+	//
+	private boolean hovered, pressed, dragging;
+	public boolean isHovered() { return hovered; }
+	public boolean isPressed() { return pressed; }
+	public boolean isDragging() { return dragging; }
+	// mouseDrag start x, y
+	private int mdsx, mdsy;
+	protected int getMouseDragStartX() { return mdsx; }
+	protected int getMouseDragStartY() { return mdsy; }
+	// last mouse x, y
+	private int lmx, lmy;
+	// last mouse hovered?
+	private boolean lmh;
+	
+	private boolean enabled;
+	public boolean isEnabled() { return enabled; }
+	public void enable() { enabled=true; }
+	public void disable() { enabled=false; }
+	
+	
+	public boolean contains(int x, int y)
+	{
+		if(x<this.x) return false;
+		if(x>this.x+this.w) return false;
+		if(y<this.y) return false;
+		if(y>this.y+this.h) return false;
+		return true;
+	}
+	
+	public final void processMouseEvent(MouseEvent ev)
+	{
+		if(this.contains(ev.getMouseX(), ev.getMouseY()))
+		{
+			hovered=true;
+		}
+		else
+		{
+			hovered=false;
+		}
+		
+		switch(ev.getType())
+		{
+		case MOTION:
+			if(pressed)
+			{
+				if(!dragging)
+				{
+					// !dragging && pressed => драг ещё не начался, но уже может - стоит проверить:
+					int dx=mdsx-ev.getMouseX(), dy=mdsy-ev.getMouseY();
+					if(dx*dx+dy*dy>25)
+					{
+						// ага, попался! провёл мышью больше 5px => это уже драг
+						dragging=true;
+						onMouseDragStart(ev);
+					}
+					else
+					{
+						// рано, рано...
+						if(hovered) onMouseMoved(ev);
+					}
+				}
+				else // dragging
+				{
+					onMouseDragging(ev);
+					if(hovered)
+					{
+						if(!lmh) onMouseOver(ev);
+					}
+					else
+					{
+						if(lmh) onMouseOut(ev);
+					}
+				}
+			}
+			else // !pressed
+			{
+				if(hovered)
+				{
+					if(lmh) onMouseMoved(ev);
+					else onMouseOver(ev);
+				}
+				else
+				{
+					if(lmh) onMouseOut(ev);
+				}
+			}
+			break;
+			
+		case PRESSED:
+			if(hovered)
+			{
+				pressed=true;
+				mdsx=ev.getMouseX();
+				mdsy=ev.getMouseY();
+				onMousePressed(ev);
+			}
+			break;
+			
+		case RELEASED:
+			if(dragging)
+			{
+				dragging=false;
+				onMouseDragEnd(ev);
+			}
+			if(hovered)
+			{
+				pressed=false;
+				onMouseReleased(ev);
+			}
+			break;
+			
+		case SCROLL:
+			if(hovered)
+			{
+				onMouseScroll(ev);
+			}
+			break;
+		}
+		
+		lmx=ev.getMouseX();
+		lmy=ev.getMouseY();
+		lmh=hovered;
+	}
+	
+	protected void onMouseMoved(MouseEvent ev){}
+	
+	protected void onMousePressed(MouseEvent ev){}
+	protected void onMouseReleased(MouseEvent ev){}
+	
+	protected void onMouseOver(MouseEvent ev){}
+	protected void onMouseOut(MouseEvent ev){}
+	
+	protected void onMouseDragging(MouseEvent ev){}
+	protected void onMouseDragStart(MouseEvent ev){}
+	protected void onMouseDragEnd(MouseEvent ev){}
+	
+	protected void onMouseScroll(MouseEvent ev){}
+}
