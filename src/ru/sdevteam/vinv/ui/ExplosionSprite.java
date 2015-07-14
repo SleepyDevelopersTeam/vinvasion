82377f9f511c5a5fd55a0442cbf39246d4688e41
Nikita <nikita-ivanov.mail@yandex.ru>
2015-07-15 00:41:47 +0300

    Created ExplosionSprite.

---
+++ src/ru/sdevteam/vinv/ui/ExplosionSprite.java
@@ -0,0 +1,50 @@
+package ru.sdevteam.vinv.ui;
+
+import java.awt.image.BufferedImage;
+
+import ru.sdevteam.vinv.game.Explosion;
+import ru.sdevteam.vinv.main.ResourceManager;
+
+public class ExplosionSprite extends Sprite
+{
+	private Explosion instance;
+	
+	
+	public ExplosionSprite(Explosion exp)
+	{
+		super(getImage(exp.getType()), getSize(exp.getType()), getSize(exp.getType()));
+		instance=exp;
+		pause();
+	}
+	
+	
+	@Override
+	public synchronized void update()
+	{
+		super.update();
+		
+		if(this.currentFrame()==this.getFramesCount()-1)
+			this.pause();
+	}
+	
+	private static BufferedImage getImage(Explosion.Type type)
+	{
+		switch(type)
+		{
+		case REGULAR:		return ResourceManager.getBufferedImage("explosions/regular");
+		case SLIME:			return ResourceManager.getBufferedImage("explosions/slime");
+		case BIG_SLIME:		return ResourceManager.getBufferedImage("explosions/big_slime");
+		}
+		return null;
+	}
+	private static int getSize(Explosion.Type type)
+	{
+		switch(type)
+		{
+		case REGULAR:		return 32;
+		case SLIME:			return 16;
+		case BIG_SLIME:		return 32;
+		}
+		return 0;
+	}
+}
